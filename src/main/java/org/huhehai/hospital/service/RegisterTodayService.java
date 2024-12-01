26a2ca2688d90685598c8f9b3647f2aa586c36c0
“C2921028” <2528980886@qq.com>
2024-12-01 17:55:58 +0800

    2

--- src/main/java/org/huhehai/hospital/service/RegisterTodayService.java
+++
@@ -1,9 +0,0 @@
-package org.huhehai.hospital.service;
-
-import org.huhehai.hospital.entity.RegisterToday;
-import java.util.List;
-
-public interface RegisterTodayService {
-    List<RegisterToday> getRegisterTodayByPatientName(String patient_Name);
-    void addRegisterToday(RegisterToday registerToday);
-}
